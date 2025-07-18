cb527a30f1c5db33f9bee3e7ff81b2be73ea5c08
IceTheCoder <cristian.gabriel131408@gmail.com>
2025-07-18 08:49:04 +0300

    figured out how to edit the js

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
+const firebaseApp = initializeApp({
+//   file deepcode ignore HardcodedNonCryptoSecret: it's safe to include the firebase config file api key in the client side
+  apiKey: "AIzaSyBWn8uw4FvMMJEuueTpKWBsP61e8vWNAws",
+  authDomain: "pet-grabber.firebaseapp.com",
+  projectId: "pet-grabber",
+  storageBucket: "pet-grabber.firebasestorage.app",
+  messagingSenderId: "368088508063",
+  appId: "1:368088508063:web:1bb6619fedc74aa4e964aa",
+  measurementId: "G-QH99SWQXJS"
+})
+const auth = getAuth(firebaseApp);
+
+// Detect auth state
+onAuthStateChanged(auth, user => {
+  if (user !== null) {
+    console.log("Logged in!");
+  } else {
+    console.log("No user!");
+  }
+});
