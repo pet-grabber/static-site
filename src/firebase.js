4e865255036234b3711dcfd70798cb21f1fcd4ac
IceTheCoder <cristian.gabriel131408@gmail.com>
2025-07-18 13:45:10 +0300

    done

---
+++ src/firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  connectAuthEmulator,
+  browserSessionPersistence,
+  setPersistence
+} from 'firebase/auth';
+
+const firebaseApp = initializeApp({
+//   file deepcode ignore HardcodedNonCryptoSecret: it's safe to include the firebase config file api key in the client side
+  apiKey: "AIzaSyBWn8uw4FvMMJEuueTpKWBsP61e8vWNAws",
+  authDomain: "pet-grabber.firebaseapp.com",
+  projectId: "pet-grabber",
+  storageBucket: "pet-grabber.firebasestorage.app",
+  messagingSenderId: "368088508063",
+  appId: "1:368088508063:web:1bb6619fedc74aa4e964aa",
+  measurementId: "G-QH99SWQXJS"
+})
+
+const auth = getAuth(firebaseApp);
+// connectAuthEmulator(auth, "http://localhost:9099");
+
+setPersistence(auth, browserSessionPersistence);
+
+export { auth };
