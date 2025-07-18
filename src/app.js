2ebe04a92ad75d25683d7bddbae0683fb679e569
IceTheCoder <cristian.gabriel131408@gmail.com>
2025-07-18 13:43:41 +0300

    FINALLY: you can log into a page

--- src/app.js
+++ src/app.js
@@ -1,4 +1,14 @@
+import { auth } from './firebase.js';
+import { onAuthStateChanged } from 'firebase/auth';
+
 const loadApp = () => {
   alert("Loading app.");
 }
 
+onAuthStateChanged(auth, user => {
+  if (user) {
+    document.body.style.display = 'block';
+  } else {
+    window.location.href = "/login.html";
+  }
+})
