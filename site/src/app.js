9ab8e08d9eb8fe70c1be7a771185f9ae11dfd2fe
IceTheCoder <cristian.gabriel131408@gmail.com>
2025-07-25 18:47:33 +0300

    remove random console.log

--- site/src/app.js
+++ site/src/app.js
@@ -135,7 +135,6 @@ function gripFunction() {
 }
 
 function writeUserData(directie, umar, brat, cleste) {
-  console.log(direction);
   if (directie === undefined) directie = null;
   if (umar === undefined) umar = null;
   if (brat === undefined) brat = null;
